d735628f765c07fbe78162b749ee0ec6c6b03e06
Mayank-3e <7mayank.agarwal@gmail.com>
2023-01-29 16:16:15 +0530

    L273 finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
 
 const HomePage = () => {
   return (
-    <h1>My home page</h1>
+    <>
+      <h1>My home page</h1>
+      <p>Go to <Link to="/products">the list of products</Link></p>
+    </>
   )
 }
 
