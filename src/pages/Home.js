42bec088a8a3ca8de994cf94b46102c53b3083b9
Mayank-3e <7mayank.agarwal@gmail.com>
2023-01-31 15:58:22 +0530

    L276 finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,23 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from 'react-router-dom';
 
-const HomePage = () => {
+function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate('/products');
+  }
+
   return (
     <>
-      <h1>My home page</h1>
-      <p>Go to <Link to="/products">the list of products</Link></p>
+      <h1>My Home Page</h1>
+      <p>
+        Go to <Link to="/products">the list of products</Link>.
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
+      </p>
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
