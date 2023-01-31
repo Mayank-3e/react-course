fa1ad2d4a3e9898de0a8fc5c9759186b78e0d1aa
Mayank-3e <7mayank.agarwal@gmail.com>
2023-01-31 16:16:44 +0530

    L280 finished

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const router=createBrowserRouter([
     errorElement: <ErrorPage/>,
     children:
     [
-      {path: '',element: <HomePage/>},
+      {index: true,element: <HomePage/>},
       {path: 'products',element: <Products/>},
       {path: 'products/:productId',element: <ProductDetailPage/>}
     ]
