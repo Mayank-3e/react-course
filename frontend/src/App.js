d22d9946e4c16a5683388e87e0db4e6ddaf5f89c
Mayank-3e <7mayank.agarwal@gmail.com>
2023-02-03 15:45:47 +0530

    L285 finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import EditEventPage from './pages/EditEvent';
 import EventDetailPage from './pages/EventDetail';
 import EventsPage from './pages/Events';
@@ -16,19 +17,35 @@ const router = createBrowserRouter([
       {
         path: 'events',
         element: <EventsRootLayout />,
-        children: [
-          { index: true, element: <EventsPage /> },
+        children:
+        [
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: async () =>
+            {
+              const response = await fetch('http://localhost:8080/events')
+              if (!response.ok) {
+                // ...
+              }
+              else
+              {
+                const resData = await response.json();
+                return resData.events;
+              }
+            }
+          },
           { path: ':eventId', element: <EventDetailPage /> },
           { path: 'new', element: <NewEventPage /> },
           { path: ':eventId/edit', element: <EditEventPage /> },
         ],
       },
     ],
-  }
+  },
 ]);
 
 function App() {
   return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
