9ce5706bd1598923c7db8188ee95b833f0ca7e4b
Mayank-3e <7mayank.agarwal@gmail.com>
2023-02-03 23:13:27 +0530

    L295 finished

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,14 +1,22 @@
-import { useParams } from 'react-router-dom';
+import EventItem from "../components/EventItem";
+import {json, useLoaderData} from 'react-router-dom'
 
-function EventDetailPage() {
-  const params = useParams();
-
-  return (
-    <>
-      <h1>EventDetailPage</h1>
-      <p>Event ID: {params.eventId}</p>
-    </>
-  );
+function EventDetailPage()
+{
+  const data=useLoaderData()
+  
+  return <EventItem event={data.event} />
 }
+export default EventDetailPage
 
-export default EventDetailPage;
+export async function loader({params})
+{
+  const id = params.eventId;
+
+  const response = await fetch('http://localhost:8080/events/' + id)
+
+  if (response.ok) return response
+  throw json(
+    {message: 'Could not fetch details for selected event.'},
+    {status: 500})
+}
