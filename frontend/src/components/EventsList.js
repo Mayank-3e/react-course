b7c5555a302e8ebf40cbff32c68fbaf801df4aac
Mayank-3e <7mayank.agarwal@gmail.com>
2023-02-03 15:51:58 +0530

    L286 finished

--- frontend/src/components/EventsList.js
+++ frontend/src/components/EventsList.js
@@ -1,6 +1,7 @@
 import classes from './EventsList.module.css';
 
-function EventsList({ events }) {
+function EventsList({events})
+{
   return (
     <div className={classes.events}>
       <h1>All Events</h1>
@@ -18,7 +19,7 @@ function EventsList({ events }) {
         ))}
       </ul>
     </div>
-  );
+  )
 }
 
 export default EventsList
