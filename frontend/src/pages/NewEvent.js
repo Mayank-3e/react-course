163860311a4f4b61cd95d7424ee80c3b7d0d3364
Mayank-3e <7mayank.agarwal@gmail.com>
2023-02-04 15:44:21 +0530

    L297 finished

--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,5 +1,8 @@
-function NewEventPage() {
-  return <h1>NewEventPage</h1>;
+import EventForm from "../components/EventForm"
+
+function NewEventPage()
+{
+  return <EventForm/>
 }
 
-export default NewEventPage;
+export default NewEventPage
