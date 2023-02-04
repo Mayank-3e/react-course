badababf2b963df09a90dd7532c0a66d5c1e4528
Mayank-3e <7mayank.agarwal@gmail.com>
2023-02-04 22:57:27 +0530

    L303 finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,61 +2,68 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import EditEventPage from './pages/EditEvent';
 import ErrorPage from './pages/Error';
-import EventDetailPage,
-{
+import EventDetailPage, {
   loader as eventDetailLoader,
-  action as deleteEventAction
+  action as deleteEventAction,
 } from './pages/EventDetail';
-import EventsPage, {loader as eventsLoader} from './pages/Events';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventsRootLayout from './pages/EventsRoot';
 import HomePage from './pages/Home';
 import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
-    errorElement: <ErrorPage/>,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
         path: 'events',
         element: <EventsRootLayout />,
-        children:
-        [
+        children: [
           {
             index: true,
             element: <EventsPage />,
-            loader: eventsLoader
+            loader: eventsLoader,
           },
           {
             path: ':eventId',
             id: 'event-detail',
             loader: eventDetailLoader,
-            children:
-            [
+            children: [
               {
                 index: true,
                 element: <EventDetailPage />,
-                action: deleteEventAction
+                action: deleteEventAction,
               },
-              { path: 'edit',
+              {
+                path: 'edit',
                 element: <EditEventPage />,
-                action: manipulateEventAction
+                action: manipulateEventAction,
               },
-            ]
+            ],
           },
-          { path: 'new', element: <NewEventPage />, action: manipulateEventAction },
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
         ],
+      },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
       },
     ],
   },
 ]);
 
-function App()
-{
+function App() {
   return <RouterProvider router={router} />;
 }
 
