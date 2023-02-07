3c5f54306385b94e12fb0f5caa51a523c1790665
Mayank-3e <7mayank.agarwal@gmail.com>
2023-02-07 23:14:25 +0530

    L371 finished

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { lazy, Suspense } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
+import HomePage from './pages/Home';
+// import PostPage, { loader as postLoader } from './pages/Post';
+import RootLayout from './pages/Root';
+
+const BlogPage=lazy(()=>import('./pages/Blog'))
+const PostPage=lazy(()=>import('./pages/Post'))
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'posts',
+        children:
+        [
+          { index: true,
+            element:
+              <Suspense fallback={<p>Loading...</p>}>
+                <BlogPage />
+              </Suspense>,
+            loader: async()=>
+            {
+              const module=await import('./pages/Blog')
+              return module.loader()
+            }
+          },
+          { path: ':id',
+            element:
+            <Suspense fallback={<p>Loading...</p>}>
+              <PostPage />
+            </Suspense>,
+            loader: async(meta)=>
+            {
+              const module=await import('./pages/Post')
+              return module.loader(meta)
+            }
+          }
+        ],
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
