a6c747b2550b3d47eb5b5dfc42904401fd833c5c
Mayank-3e <7mayank.agarwal@gmail.com>
2023-02-09 16:21:45 +0530

    L393 finished

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,5 +1,14 @@
+import { useRouter } from 'next/router';
+
+// our-domain.com/news/something-important
+
 function DetailPage() {
-    return <h1>The Detail Page</h1>
-  }
-  
-  export default DetailPage;
+  const router = useRouter();
+
+  const newsId = router.query.newsId
+  console.log(newsId)
+
+  return <h1>The Detail Page</h1>
+}
+
+export default DetailPage;
