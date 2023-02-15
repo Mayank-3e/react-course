f321d3c2963fbc36d4769192f0bbdd146629a704
Mayank-3e <7mayank.agarwal@gmail.com>
2023-02-15 21:32:51 +0530

    L408 finished

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,7 +1,18 @@
+import { MongoClient } from "mongodb"
+
 export default async function handler(req, res)
 {
   if (req.method === 'POST')
   {
-    const {title,image,address,description} = req.body
+    const data = req.body
+
+    const client = await MongoClient.connect(`mongodb+srv://${process.env.username}:${process.env.password}@cluster0.szwbefi.mongodb.net/meetups?retryWrites=true&w=majority`)
+    const db = client.db()
+    const meetupsCollection = db.collection('meetups')
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+    client.close();
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
