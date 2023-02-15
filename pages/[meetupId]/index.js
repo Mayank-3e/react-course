6da00bc6dfb349a5ec027b2887d54e82ab05302d
Mayank-3e <7mayank.agarwal@gmail.com>
2023-02-15 14:22:53 +0530

    L405 finished

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,22 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticProps({params})
+{
+  const meetupId = params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        id: meetupId,
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
+      },
+    },
+  };
+}
+
 export default MeetupDetails
