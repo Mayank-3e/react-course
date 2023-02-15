f32d0aed4e44dd100d1541be531786bf31fb85fa
Mayank-3e <7mayank.agarwal@gmail.com>
2023-02-15 14:28:20 +0530

    L406 finished

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,6 +11,26 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths()
+{
+  return {
+    fallback: false,
+    paths:
+    [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ]
+  }
+}
+
 export async function getStaticProps({params})
 {
   const meetupId = params.meetupId;
